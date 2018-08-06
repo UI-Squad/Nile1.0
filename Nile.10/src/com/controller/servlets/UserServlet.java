27777549a0e9f0510710d494361c855f909b81f8
Shane <Shane@Shane-PC>
2018-08-06 16:56:34 -0500

    additional functionality

--- Nile.10/src/com/controller/servlets/UserServlet.java
+++ Nile.10/src/com/controller/servlets/UserServlet.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -31,6 +33,8 @@ public class UserServlet extends HttpServlet {
 	private PrintWriter write;
 	private Boolean logged = false;
 	private Object obj = null;
+	private String email;
+	private String password;
 	
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{  
     	doProcess(request, response);
@@ -45,110 +49,27 @@ public class UserServlet extends HttpServlet {
     	setConnector(session);
     	control = new Controller(connector);
     	String page = (String)request.getParameter("page");
-    	String itemId = null;
-    	String email = null;
-    	String password = null;
-    	int qty = 0;
+    	email = null;
+    	password = null;
     	try {
     		switch(page) {
     		case "login":
-    			write = response.getWriter();
-    			email = request.getParameter("email");
-    			password = request.getParameter("password");
-    			if(email != null && password != null) { //existing user
-    				user = control.customerHandle().getByEmail(request.getParameter("email"),
-							request.getParameter("password"));
-    				if(user == null) { //login failed
-    					write.println("<script type=\"text/javascript\">");  
-    					write.println("alert('Invalid email/password. Please try again.');");  
-    					write.println("window.location.replace(\"login.jsp\");");
-    					write.println("</script>"); 
-    				}
-    				if(user.getCart().getSize() > 0) {
-    					control.cartHandle().returnCart(user.getCart().getCartId());	
-    				}
-    			}else { //first time login
-    				setUser(session);
-    			}
-    			System.out.println("LOGIN " + user.getId() + " CONFIRMED");	
-    			session.setAttribute("user", user);
-    			logged = true;
-    			session.setAttribute("logged", logged);
-    			session.setAttribute("cart", user.getCart());
-    			request.getRequestDispatcher("Website.jsp").forward(request, response);
+    			login(request, response);
     			break;
     		case "register":
-    			password = request.getParameter("psw");
-    			String repassword = request.getParameter("psw-repeat"); 
-    			verifyPw(password, repassword);
-    			String firstNm =request.getParameter("FirstName");
-    			String lastNm = request.getParameter("LastName");
-    			email = request.getParameter("email");
-    			System.out.println("email" + email);
-    			System.out.println("password" + password);
-    			System.out.println("firstNm" + firstNm);
-    			System.out.println("lastNm" + lastNm);
-    			control.customerHandle().add(email, repassword, firstNm, lastNm);
-    			System.out.println("ADDED TO DATABASE");
-    			user = control.customerHandle().getByEmail(email, repassword);
-    			System.out.println(user.toString());
-    			session.setAttribute("user", user);
-    			session.setAttribute("cart", user.getCart());	
-    			request.getRequestDispatcher("user?page=login").forward(request, response);
+    			register(request, response);
     			break;
     		case "cart":	//cart page
-    			setUser(session);
-    			session.setAttribute("cart", user.getCart());
-    			//obj = request.getAttribute("logged");
-    			//logged = (obj != null) ? (Boolean)obj : false;
-    			request.getRequestDispatcher("cart.jsp").forward(request, response);
+    			displayCart(request, response);
     			break;
     		case "additem": //add item to cart
-    			setUser(session);
-    			//obj = request.getAttribute("logged");
-    			//logged = (obj != null) ? (Boolean)obj : false;
-    			itemId = request.getParameter("itemID");
-    			qty = Integer.parseInt(request.getParameter("numberOfItem"));
-    			Item item = control.inventoryHandle().getItemById(itemId);
-    			if(item != null) { //null check
-    				if(item.getQuantity() >= qty) { //quantity check
-    					//add item to user's cart
-    					item.setQuantity(qty);
-    					user.getCart().addToCart(item);
-    					control.inventoryHandle().subtractQuantity(itemId, qty);
-    	    			//add cart item to DB
-    	    			control.cartHandle().addItem(user.getCart().getCartId(), 
-    	    					user.getId(), itemId, qty);
-    	    			System.out.println("ITEM: " + item.getItemName() + " ADDED TO CART");
-    				}
-    			}
-    			session.setAttribute("cart", user.getCart());
-    			request.getRequestDispatcher("cart.jsp").forward(request, response);
+    			addItem(request, response);
     			break;
     		case "removeitem":
-    			setUser(session);
-    			//get itemId and Quantity
-    			itemId = request.getParameter("itemID");
-    			System.out.println("REMOVING: " + itemId);
-    			qty = user.getCart().getItem(itemId).getQuantity();
-    			System.out.println("QUANTITY: " + qty);
-    			//remove from cart and add back into inventory
-    			control.cartHandle().removeItem(user.getCart().getCartId(), itemId);
-    			user.getCart().removeItem(itemId);
-    			control.inventoryHandle().addQuantity(itemId, qty);
-    			//forward back to cart page
-    			System.out.println("ITEM: " + itemId + " REMOVE FROM CART");
-    			session.setAttribute("cart", user.getCart());
-    			request.getRequestDispatcher("cart.jsp").forward(request, response);
+    			removeItem(request, response);
     			break;
     		case "logout": //logout
-    			logged = false;
-    			session.setAttribute("logged", logged);
-    			connector.closeConnection();
-    			session.invalidate();
-    			System.out.println("USER " + user.getId() + " LOGGED OUT");
-    			session.setAttribute("cart", null);
-    			response.sendRedirect("Website.jsp");
+    			logout(request, response);
     			break;
     		default:
     			System.err.println("USER DISPATCHER ERROR");
@@ -208,6 +129,107 @@ public class UserServlet extends HttpServlet {
     	}
 		System.out.println("USERID:" + user.getId());
 		System.out.println("CARTID:" + user.getCart().getCartId());
+    }
+    
+    private void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		write = response.getWriter();
+		email = request.getParameter("email");
+		password = request.getParameter("password");
+		if(email != null && password != null) { //existing user
+			user = control.customerHandle().getByEmail(request.getParameter("email"),
+					request.getParameter("password"));
+			if(user == null) { //login failed
+				write.println("<script type=\"text/javascript\">");  
+				write.println("alert('Invalid email/password. Please try again.');");  
+				write.println("window.location.replace(\"login.jsp\");");
+				write.println("</script>"); 
+			}
+			if(user.getCart().getSize() > 0) {
+				control.cartHandle().returnCart(user.getCart().getCartId());	
+			}
+		}else { //first time login
+			setUser(session);
+		}
+		System.out.println("LOGIN " + user.getId() + " CONFIRMED");	
+		session.setAttribute("user", user);
+		logged = true;
+		session.setAttribute("logged", logged);
+		session.setAttribute("cart", user.getCart());
+		request.getRequestDispatcher("Website.jsp").forward(request, response);
+    }
+    
+    
+    
+    private void register(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		password = request.getParameter("psw");
+		String repassword = request.getParameter("psw-repeat"); 
+		verifyPw(password, repassword);
+		String firstNm =request.getParameter("FirstName");
+		String lastNm = request.getParameter("LastName");
+		email = request.getParameter("email");
+		System.out.println("email" + email);
+		System.out.println("password" + password);
+		System.out.println("firstNm" + firstNm);
+		System.out.println("lastNm" + lastNm);
+		control.customerHandle().add(email, repassword, firstNm, lastNm);
+		System.out.println("ADDED TO DATABASE");
+		user = control.customerHandle().getByEmail(email, repassword);
+		System.out.println(user.toString());
+		session.setAttribute("user", user);
+		session.setAttribute("cart", user.getCart());	
+		request.getRequestDispatcher("user?page=login").forward(request, response);
+    }
+    
+    private void addItem(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			setUser(session);
+			String itemId = request.getParameter("itemID");
+			int qty = Integer.parseInt(request.getParameter("numberOfItem"));
+			Item item = control.inventoryHandle().getItemById(itemId);
+			if(item != null) { //null check
+				if(item.getQuantity() >= qty) { //quantity check
+					//add item to user's cart
+					item.setQuantity(qty);
+					user.getCart().addToCart(item);
+					control.inventoryHandle().subtractQuantity(itemId, qty);
+	    			//add cart item to DB
+	    			control.cartHandle().addItem(user.getCart().getCartId(), 
+	    					user.getId(), itemId, qty);
+	    			System.out.println("ITEM: " + item.getItemName() + " ADDED TO CART");
+				}
+			}
+			session.setAttribute("cart", user.getCart());
+			request.getRequestDispatcher("cart.jsp").forward(request, response);
+    }
+    
+    private void removeItem(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		setUser(session);
+		//get itemId and Quantity
+		String itemId = request.getParameter("itemID");
+		System.out.println("REMOVING: " + itemId);
+		int qty = user.getCart().getItem(itemId).getQuantity();
+		System.out.println("QUANTITY: " + qty);
+		//remove from cart and add back into inventory
+		control.cartHandle().removeItem(user.getCart().getCartId(), itemId);
+		user.getCart().removeItem(itemId);
+		control.inventoryHandle().addQuantity(itemId, qty);
+		//forward back to cart page
+		System.out.println("ITEM: " + itemId + " REMOVE FROM CART");
+		session.setAttribute("cart", user.getCart());
+		request.getRequestDispatcher("cart.jsp").forward(request, response);
+    }
+    
+    private void displayCart(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		setUser(session);
+		session.setAttribute("cart", user.getCart());
+		request.getRequestDispatcher("cart.jsp").forward(request, response);
+    }
+    
+    private void logout(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		logged = false;
+		connector.closeConnection();
+		session.invalidate();
+		System.out.println("USER " + user.getId() + " LOGGED OUT");
+		response.sendRedirect("Website.jsp");
     }
     
 	/**
